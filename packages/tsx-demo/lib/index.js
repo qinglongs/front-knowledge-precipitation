dba997ec1a621c64a6c32ab4c36dfd4a3f9940f8
李清龙 <924229679@qq.com>
2021-07-22 20:51:34 +0800

    add antd

--- packages/tsx-demo/lib/index.js
+++ packages/tsx-demo/lib/index.js
@@ -1,13 +1,18 @@
 'use strict';
 
 var React = require('react');
+var antd = require('antd');
 
 function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
 var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
 
 var TsxDemo = function () {
-    return React__default['default'].createElement("h1", null, "111222");
+    var _a = React.useState(1), count = _a[0], setCount = _a[1];
+    var onClickButton = function () {
+        setCount(count + 1);
+    };
+    return React__default['default'].createElement(antd.Button, { type: "primary", onClick: onClickButton }, "111222");
 };
 
 module.exports = TsxDemo;
